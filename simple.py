e251b1f66c94192f9b8d5f612a1ab20588a894b0
lyanevgeny <9615499@gmail.com>
2021-03-16 13:20:19 +0100

    introducing of get_channels as a function:  - get all channels as numpy matrix

--- simple.py
+++ simple.py
@@ -1,30 +1,39 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pywt
 
 def get_channels(filename):
+    # returns the matrix of all channels and axillary info
+
     # Open a file
     file = open ('ecg.txt', 'r')
     lines = file.readlines ()
-
+    number_of_channels = 28             # TODO write a code to infer number of channels and samples
+    samples_per_channel = 60000         # TODO write a code to infer the labels for channels
     # Search for data
     count = False
-    channels = []
+
+    channels = np.empty((0,number_of_channels), int) # initialize channel matrix
     for line in lines:
         if line.strip () == "[Data]":  # start processing after "[Data]" line
             count = True
             continue
         if count:
-            channels.append (line.split (',')[0])  # adding data of channel with index [1] to array
-
-    return channels
+            # stripping the line (current sample for all channels), into np 1-D array
+            newline = np.fromstring(line, dtype=int, sep=',')
+            # adding data of the current sample to channel matrix
+            channels = np.append(channels,[newline], axis=0)
+    # transpose channel matrix and return
+    return number_of_channels, samples_per_channel, channels.T
 
 def main():
 
-    data = get_channels('ecg.txt')
+    number_of_channels, samples_per_channel, channels = get_channels('ecg.txt')
+    print(channels.shape)
 
     # Filter
-    wavelets = pywt.wavedec (data, 'db4', level=3)
-    print (len (wavelets))
+    wavelets = pywt.wavedec (channels[0], 'db4', level=3)
+
 
     # Output
     plt.plot (wavelets[0])
