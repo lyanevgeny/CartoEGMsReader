fd7f45134387aafd82b1c9d77c6137c0dfe0a38c
lyanevgeny <9615499@gmail.com>
2021-03-16 14:30:52 +0100

    introducing of get_channels as a function:  - get all channels as numpy matrix  - plot all channels # looks good even w/o Filter

--- simple.py
+++ simple.py
@@ -14,7 +14,7 @@ def get_channels(filename):
     count = False
 
     channels = np.empty((0,number_of_channels), int) # initialize channel matrix
-    for line in lines:
+    for i, line in enumerate(lines):
         if line.strip () == "[Data]":  # start processing after "[Data]" line
             count = True
             continue
@@ -23,21 +23,36 @@ def get_channels(filename):
             newline = np.fromstring(line, dtype=int, sep=',')
             # adding data of the current sample to channel matrix
             channels = np.append(channels,[newline], axis=0)
+            if i > 10000:
+                break
+
     # transpose channel matrix and return
     return number_of_channels, samples_per_channel, channels.T
 
+def plot_channels(channels):
+    # Visualization of all channels
+    fig = plt.figure ()
+    gs = fig.add_gridspec (len(channels), hspace=0)
+    axs = gs.subplots (sharex=True)
+    fig.suptitle ('All channels:')
+    for i, channel in enumerate(channels):
+        # axs[i].plot (pywt.wavedec(channel, 'db4', level=7)[0])   - seems we don't need the Filter
+        axs[i].plot (channel)                                       # looks fine without Filter
+
+        # axs[i].set_title ("Channel #")
+        # axs[i].get_xaxis ().set_visible (True)
+        axs[i].get_yaxis ().set_visible (False)
+    # plt.grid(True)
+    plt.show()
+
+
+
 def main():
 
-    number_of_channels, samples_per_channel, channels = get_channels('ecg.txt')
-    print(channels.shape)
-
-    # Filter
-    wavelets = pywt.wavedec (channels[0], 'db4', level=3)
-
+    number_of_channels, samples_per_channel, raw_channels = get_channels('ecg.txt')
 
     # Output
-    plt.plot (wavelets[0])
-    plt.show ()
+    plot_channels(raw_channels)
 
 
 if __name__ == '__main__':
